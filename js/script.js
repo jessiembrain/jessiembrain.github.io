acb70b404bf32bcdb1151f207edc5da3fab680d9
Jessie Brain <105239826+jessiembrain@users.noreply.github.com>
2026-05-05 16:29:28 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,27 +7,7 @@
   function initializeMap() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiamJyYWluMSIsImEiOiJjbG85MTNmZW8wNW80MnFwbTRiZXJmNGZuIn0.4Ajv4kTvH6rx0ym0AmE-gQ';
 
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/jbrain1/cmoszsu3g001j01s473gx6xzs',
-      center: [-75.19406830626386, 39.96963965399903],
-      zoom: 2,
-      interactive: false,
-      attributionControl: false
-    });
-
-    map.on('load', () => {
-      map.setTerrain(null);
-      flyNorth(map);
-      map.flyTo({
-        center: [-75.19406830626386, 39.96963965399903],
-        zoom: 16,
-        pitch: 100,
-        speed: 0.5,
-        curve: 1,
-        essential: true
-      });
-
+    const map = new mapboxgl.Map({ container: 'map', style: 'mapbox://styles/jbrain1/cmoszsu3g001j01s473gx6xzs', center: [-75.16368012081547, 39.952484115995304], zoom: 2, interactive: false, attributionControl: false }); map.on('load', () => { map.setTerrain(null); map.flyTo({ center: [-75.16368012081547, 39.952484115995304], zoom: 16, pitch: 75, speed: 0.5, curve: 3, essential: true }); map.once('moveend', () => { map.easeTo({ pitch: 85, bearing: 0, duration: 10, easing: (t) => t, essential: true }); flyNorth(map);
       map.once('moveend', () => {
         map.easeTo({
           pitch: 90,
